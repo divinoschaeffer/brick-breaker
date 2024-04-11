2830d80e854e0a60aec9e3bb3070b13fbc5386ca
Divino Schaeffer <divino.schaeffer02@gmail.com>
2024-04-11 18:25:18 +0200

    affiche msg

--- Makefile
+++ Makefile
@@ -3,7 +3,3 @@
 all:
 	mkdir -p cmake-build-debug
 	cd cmake-build-debug && cmake .. && make
-
-clean:
-	rm -rf cmake-build-debug
-	rm BrickBreaker
---
+++ include/Rectangle.h
@@ -0,0 +1,25 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+#include <SDL2/SDL.h>
+
+class Rectangle {
+public:
+    Rectangle(SDL_Renderer* renderer, int x, int y, int w, int h);
+
+    void draw() const;
+    void setColor(unsigned char r, unsigned char g, unsigned char b, unsigned char a);
+    void setPosition(int x, int y);
+    void setSize(int w, int h);
+
+    // Méthodes getters pour obtenir la position du rectangle
+    int getX() const;
+    int getY() const;
+
+private:
+    SDL_Renderer* renderer;
+    SDL_Rect rect;
+    SDL_Color color;
+};
+
+#endif /* RECTANGLE_H */
