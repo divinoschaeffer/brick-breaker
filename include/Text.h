a45af0d617afbc47f9ba09ed6d62742f65ddc131
bdam <bdam@etu.unistra.fr>
2024-04-17 14:52:53 +0200

    J'avais oublié ce fichier là

--- include/Text.h
+++ include/Text.h
@@ -1,16 +1,21 @@
-//
-// Created by Bryan on 16/04/2024.
-//
-
 #ifndef TEXT_H
 #define TEXT_H
-
-
+#include <SDL_ttf.h>
+#include <string>
 
 class Text {
+protected:
+    SDL_Renderer* renderer;
+    SDL_Color color;
+    std::string text;
+    TTF_Font* font;
+    SDL_Surface* surface;
+    SDL_Texture* texture;
+    SDL_Rect rect;
 
+public:
+    Text(SDL_Renderer* renderer, SDL_Color color, const std::string& text, TTF_Font* font);
+    virtual void render(SDL_Renderer* renderer, const int posX, const int posY);
 };
-
-
 
 #endif //TEXT_H
