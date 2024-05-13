f238f9716bb6aa2999e02dd6e31efbf287a370b6
Divino Schaeffer <divino.schaeffer02@gmail.com>
2024-05-13 10:54:55 +0200

    enlever fichiers inutiles

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
-#include "HelloWorld.h"
 #include <GameLoop.h>
 
 int main(int argc, char const *argv[])
 {
-    //HelloWorld();
     GameLoop G;
     G.FirstPageLoop();
     return 0;
